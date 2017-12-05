6f9577e50d3bc3725ebf4997eb57f62a418d91e4
lenovo <ravichandrachakka8@gmail.com>
2017-12-05 18:29:08 +0530

    inital commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IDist } from '../../interfaces/Dist';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage{
+zonesdata: IDist[];
   constructor(public navCtrl: NavController) {
 
+    this.zonesdata= [
+        {
+            "zoneid": 7,
+            "zonename": "Bhongir",
+            "distcode":"rck"
+        },
+        {
+            "zoneid": 5,
+            "zonename": "L.B.Nagar",
+            "distcode":"rck"
+        }
+      ];
   }
 
 }
